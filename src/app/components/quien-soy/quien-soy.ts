c38755ce3ef67ad638a625bbdd3a761de83a443d
martinsznesquivel <martinesquivel3232332@gmail.com>
2026-05-07 13:24:57 -0300

    Sprint 1: Implementacion de changedetectorref para actualizar vista

--- src/app/components/quien-soy/quien-soy.ts
+++ src/app/components/quien-soy/quien-soy.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,21 +8,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './quien-soy.html',
   styleUrl: './quien-soy.css',
 })
-export class QuienSoy implements OnInit{
-
+export class QuienSoy implements OnInit {
   datosGithub: any;
 
-  constructor(private http: HttpClient) {}
-    ngOnInit(): void {
-      this.http.get('https://api.github.com/users/martinsznesquivel').subscribe({
-        next: (respuesta) => {
-          this.datosGithub = respuesta;
-        },
-        error: (err) => {
-          console.error('error', err)
-        }
-      });
-    }
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+  //ChangeDetectorRef obliga a angular a actualizar la vista (sin esto la foto del usuario no carga a no ser que presionemos dos veces quien soy)
+  ngOnInit(): void {
+    this.http.get('https://api.github.com/users/martinsznesquivel').subscribe({
+      next: (respuesta) => {
+        this.datosGithub = respuesta;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('error', err);
+      },
+    });
   }
-
-
+}
