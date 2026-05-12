0df252d508e8756c16eb253d246a8b849c358e30
Martin Hernan Esquivel <martinesquivel323232@gmail.com>
2026-05-12 15:32:29 -0300

    Sprint 2, feat: implementacion de registro con validaciones de ReactiveForm

--- src/app/components/registro/registro.ts
+++ src/app/components/registro/registro.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Validators, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule], //Importamos ReactiveFormsModule para utilizar FormControl
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
-export class Registro {}
+export class Registro {
+  formulario = new FormGroup({
+    // FormGroup para mayor escalabilidad, no itera individualmente variables sueltas, si no al objeto que contiene a todas
+    nombre: new FormControl('', {
+      validators: [Validators.minLength(3), Validators.maxLength(15), Validators.required],
+    }),
+    apellido: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    edad: new FormControl('', [Validators.required, Validators.min(18), Validators.max(99)])
+  });
+
+  // prueba en consola
+  mostrar() {
+    if (!this.formulario.valid) {
+      console.log('No valido');
+    }
+    console.log(this.formulario.value);
+  }
+}
