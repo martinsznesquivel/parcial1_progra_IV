102412d8a189a4f70048ccb018b534d552d04ee7
Martin Hernan Esquivel <martinesquivel323232@gmail.com>
2026-05-18 13:41:51 -0300

    chore: actualizado gitignore

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://quhhjhyizyhapaqalhub.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1aGhqaHlpenloYXBhcWFsaHViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MDQwMTcsImV4cCI6MjA5NDE4MDAxN30.6YeyTMrH2ekNUdN_TuvRENsrmOW5dkiGsZnjK85mTbU'
-};
